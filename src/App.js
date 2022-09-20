34ed3cfc026678c2d4fbaed5c0b46f0f943570ee
gamabarbara <barbara.cp@outlook.com>
2022-09-20 16:41:09 -0300

    Finalização do Layout

---
+++ src/App.js
@@ -0,0 +1,63 @@
+export default function App(props) {
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="content-image">
+          <img src="./assets/forca0.png" />
+        </div>
+        <div className="content-button">
+          <button className="content-button-button">Escolher Palavra</button>
+        </div>
+      </div>
+      <div className="letters">
+        <Letter name={props.name} />
+      </div>
+      <div className="input">
+        <p className="text">Já sei a palavra!</p>
+        <input></input>
+        <button>Chutar</button>
+      </div>
+    </div>
+  );
+}
+
+function Letter() {
+  const letter = [
+    { name: "A" },
+    { name: "B" },
+    { name: "C" },
+    { name: "D" },
+    { name: "E" },
+    { name: "F" },
+    { name: "G" },
+    { name: "H" },
+    { name: "I" },
+    { name: "J" },
+    { name: "K" },
+    { name: "L" },
+    { name: "M" },
+    { name: "N" },
+    { name: "O" },
+    { name: "P" },
+    { name: "Q" },
+    { name: "R" },
+    { name: "S" },
+    { name: "T" },
+    { name: "U" },
+    { name: "V" },
+    { name: "X" },
+    { name: "Y" },
+    { name: "W" },
+    { name: "Z" },
+  ];
+
+  return (
+    <div className="letters-container">
+      {letter.map((s) => (
+        <div className="letter-button">
+          <p>{s.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
