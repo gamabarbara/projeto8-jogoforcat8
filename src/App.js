21007edc31f59a760c34e8a93b449f2cd13d9d62
gamabarbara <barbara.cp@outlook.com>
2022-09-21 10:20:23 -0300

    Array de palavras

--- src/App.js
+++ src/App.js
@@ -1,64 +1,103 @@
-import forca from "./assets/forca0.png";
+import React from "react";
+import forca0 from "./assets/forca0.png";
+import forca1 from "./assets/forca1.png";
+import forca2 from "./assets/forca2.png";
+import forca3 from "./assets/forca3.png";
+import forca4 from "./assets/forca4.png";
+import forca5 from "./assets/forca5.png";
+import forca6 from "./assets/forca6.png";
+import { RandomWord } from "./palavras";
+
 export default function App(props) {
   return (
     <>
       <div className="content">
-        <div className="content-image">
-          <img src={forca} />
-        </div>
-        <div className="content-button">
-          <button className="content-button-button">Escolher Palavra</button>
+        <GameImage />
+        <ButtonGame />
+        <div className="content-gessing">
+          <RandomWord />
         </div>
       </div>
       <div className="letters">
         <Letter name={props.name} />
       </div>
-      <div className="input">
-        <p className="text">Já sei a palavra!</p>
-        <input></input>
-        <button>Chutar</button>
-      </div>
+      <GuessWord />
     </>
   );
 }
 
 function Letter() {
-  const letter = [
-    { name: "A" },
-    { name: "B" },
-    { name: "C" },
-    { name: "D" },
-    { name: "E" },
-    { name: "F" },
-    { name: "G" },
-    { name: "H" },
-    { name: "I" },
-    { name: "J" },
-    { name: "K" },
-    { name: "L" },
-    { name: "M" },
-    { name: "N" },
-    { name: "O" },
-    { name: "P" },
-    { name: "Q" },
-    { name: "R" },
-    { name: "S" },
-    { name: "T" },
-    { name: "U" },
-    { name: "V" },
-    { name: "X" },
-    { name: "Y" },
-    { name: "W" },
-    { name: "Z" },
+  const alfabeto = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
   ];
 
   return (
     <div className="letters-container">
-      {letter.map((s) => (
-        <div className="letter-button">
-          <p>{s.name}</p>
+      {alfabeto.map((a) => (
+        <div>
+          <button className="letter-button">{a.toUpperCase()}</button>
         </div>
       ))}
     </div>
   );
+}
+
+function GameImage() {
+  return (
+    <div className="content-image">
+      <img src={forca0} />
+    </div>
+  );
+}
+
+function ButtonGame() {
+  return (
+    <div className="content-button">
+      <button className="content-button-button" onClick={RandomWord}>Escolher Palavra</button>
+    </div>
+  );
+}
+
+function GuessWord() {
+  return (
+    <div className="input">
+      <p className="text">Já sei a palavra!</p>
+      <input />
+      <button className="guess-button">Chutar</button>
+    </div>
+  );
+}
+
+function StepsGame(props) {
+  const [errors, setErrors] = React.useState(0)
+  const [word, setWord] = React.useState()
+  const steps = {
+    maxErrors: 6, 
+    steps: [forca0, forca1, forca2, forca3, forca4, forca5, forca6]
+  }
 }
